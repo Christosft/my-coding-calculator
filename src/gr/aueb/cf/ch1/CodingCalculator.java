dbceb90695028ef723c79f3f0fd8d5c6b336b639
Chris Ftoulis <christosftoulis3@gmail.com>
2024-12-17 19:10:11 +0200

    mod method created

--- src/gr/aueb/cf/ch1/CodingCalculator.java
+++ src/gr/aueb/cf/ch1/CodingCalculator.java
@@ -19,4 +19,8 @@ public class CodingCalculator {
     public static int div(int a, int b) {
         return a / b;
     }
+
+    public static int mod(int a, int b) {
+        return a % b;
+    }
 }
